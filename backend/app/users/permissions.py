678dd582a458d15fecd16c41b0735f58e823f322
den-sad <den-sad@yandex.ru>
2023-06-18 16:09:20 +0300

    edit for PEP8

--- backend/app/users/permissions.py
+++ backend/app/users/permissions.py
@@ -16,5 +16,4 @@ class IsAuthorOrAdmin(permissions.BasePermission):
             request.method in permissions.SAFE_METHODS or
             obj.author == request.user or
             request.user.is_admin
-
         )
